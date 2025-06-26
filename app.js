05fc9e35fc5714b58c506e16e435651cbf830f07
PRASHANT1603 <136425600+PRASHANT1603@users.noreply.github.com>
2025-06-26 10:49:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -76,6 +76,20 @@ app.post("/listings", async(req, res)=>{
 
 });
 
+// edit
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
 
 
 
